2a2952f2d93d2da2e1c429f730e5f63c27836da2
Florian <florian.guinard@etudiant.univ-rennes2.fr>
2022-06-17 20:17:25 +0200

    maj

--- Question2.py
+++ Question2.py
@@ -181,8 +181,8 @@ div = Div(text="""
 On a visualisé les liens entre les auteurs de ces publications, en utilisant un code couleur qui permette de distinguer les auteurs par
 leurs nombres de publications et en représentant les liens (co-publications) existant entre les auteurs. 
 </p>
-<a href="./index.html">
-<img src="img_accueil.png" alt="HTML tutorial" >
+<a href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_images_link">
+<img src="img_accueil.png" alt="retour accueil" >
 </a>
 
 """)
