63187352fcf84213afb300052bcf12bcc1b8065e
Florian <florian.guinard@etudiant.univ-rennes2.fr>
2022-06-18 15:30:30 +0200

    maj

--- food.py
+++ food.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 29 21:18:59 2022
-
-@author: Andrea Rogel
-"""
-
+from turtle import width
 from bokeh.plotting import figure, show, output_file
 from bokeh.io import export_png
 import numpy as np
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from bokeh.plotting import ColumnDataSource
+from bokeh.models import HoverTool, Column, Div,Row
+from bokeh.models.widgets import Tabs, Panel
 
 output_file("food.html")
 
@@ -36,14 +33,32 @@ nb = []
 for i in a:
     note.append(i['_id']) 
     nb.append(i['nb_notes'])
- 
-graph = figure(x_range = note, height = 250, 
-               title = "Nombre de notes données dans le quartier de Manhattan",
-               toolbar_location = None, tools=""    )
-   
-graph.vbar(x=note, top = nb, width = 0.5)
 
-show(graph)
-output_file("food.html")
+data = ColumnDataSource(data=dict(nombre=nb, note=note))
 
-export_png(graph, filename="food.png")
+
+
+fig = figure(title="Nombre de notes données dans le quartier de Manhattan", x_range=note, height = 400)
+
+
+fig.vbar(x='note',top='nombre',source = data, width=0.5)
+
+
+hover_tool = HoverTool(tooltips=[("Nombre de notes", "@nombre")])
+fig.add_tools(hover_tool)
+
+div = Div(text="""
+<h1> Visualisation libre de données issues de NYfood </h1>
+<p> Ce graphique représente le nombre de notes de chaque type données sur les restaurants du quartier de Manhattan à New York. 
+</p>
+
+""")
+
+layout = Row(Column(div, fig, width=900))
+output_file("question3.html")
+show(layout)
+
+
+
+
+
